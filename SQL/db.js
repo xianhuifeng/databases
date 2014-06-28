2ab2829be6fe66e90e6007b45a031abb95b6da52
Hack Reactor Students <xianhui.feng100@gmail.com>
2014-06-27 21:01:52 -0700

    add db.js

--- SQL/db.js
+++ SQL/db.js
@@ -20,7 +20,17 @@ dbConnection.connect(function(err){
   console.log('connected');
 });
 
-var selectUsers = function(callback) {
+exports.insertUser = insertUser = function(user , callback){
+  dbConnection.query('INSERT INTO user SET ?', user, callback);
+};
+exports.insertMessage = insertMessage = function(message , callback){
+  dbConnection.query('INSERT INTO message SET ?', message, callback);
+};
+exports.insertRoom = insertRoom = function(room , callback){
+  dbConnection.query('INSERT INTO room SET ?', room, callback);
+};
+
+exports.selectUsers = selectUsers = function(callback) {
   // callback takes err & rows -- callback(err, rows)
   dbConnection.query('select * from user', callback);
   //   function(err, rows) {
@@ -33,9 +43,20 @@ var selectUsers = function(callback) {
   // });
 };
 
+exports.selectMessages = selectMessages = function(callback) {
+  // callback takes err & rows -- callback(err, rows)
+  dbConnection.query('select * from message', callback);
+
+};
+
+exports.selectRooms = selectRooms = function(callback) {
+  // callback takes err & rows -- callback(err, rows)
+  dbConnection.query('select * from room', callback);
+
+};
+
 /* Now you can make queries to the Mysql database using the
  * dbConnection.query() method.
  * See https://github.com/felixge/node-mysql for more details about
  * using this module.*/
 
-exports.selectUsers = selectUsers;
